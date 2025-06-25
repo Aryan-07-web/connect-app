e5e93f51296e970a25ad3c0fc763a9095d865a26
Aryan-07-web <aryan23146@iiitd.ac.in>
2025-06-25 14:20:07 +0530

    Add files via upload
    under backend/src/lib

--- utils.js
+++ utils.js
@@ -1,7 +1,17 @@
-export function formatMessageTime(date) {
-  return new Date(date).toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false,
-  });
-}
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId, res) => {
+  // user will need to authenticate again after 7 days, since this token is generated for authentication and works only for 7 days
+  const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+    expiresIn: '7d' // Token will expire in 7 days
+  })
+
+  res.cookie("jwt",token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
+    sameSite: "Strict", // Helps prevent CSRF attacks
+    secure: process.env.NODE_ENV !== 'development' // Use secure cookies in production
+  })
+
+  return token;
+};
